bf547792df985c9aee4282cbcb425d42913de6d2
Eric Neifert <eric.neifert@gmail.com>
2014-02-05 15:24:43 +0600

    added mail route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Obtc::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get "mail", to: redirect {"https://webmail.webfaction.com/"}
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
