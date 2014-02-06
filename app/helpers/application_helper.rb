48264f23543be374490eb9928891f901b3670a48
Eric Neifert <eric.neifert@gmail.com>
2014-02-06 11:58:44 +0600

    imgs now point to kg in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def img_url_for_env name
+		if Rails.env == 'development'			
+			image_path(name)
+		else
+			"http://obtc.com.kg/obtc/images/#{name}"
+		end
+	end
 end
+	
