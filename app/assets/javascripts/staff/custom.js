fd2ac2f8acf6ac8de3c05e9f5e30a02ae0e4dff9
Eric Neifert <eric.neifert@gmail.com>
2014-02-24 14:03:25 +0600

    tightened up the page a bit

--- app/assets/javascripts/staff/custom.js
+++ app/assets/javascripts/staff/custom.js
@@ -110,7 +110,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top 
+          scrollTop: target.offset().top
         }, 1000);
         return false;
       }
